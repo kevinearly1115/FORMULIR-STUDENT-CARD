e6116afb802d0b1bca9654d01db0e6193226ab79
KEVIN E15 <kevin_eliansyah_24rpl@student.smktelkom-mlg.sch.id>
2016-09-12 13:14:26 +0700

    first commit

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl315/formulirkartupelajar/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl315/formulirkartupelajar/MainActivity.java
@@ -2,12 +2,40 @@ package id.sch.smktelkom_mlg.tugas01.xirpl315.formulirkartupelajar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    EditText etTtl;
+    EditText etAlamat;
+    Button bOK;
+    TextView tvhasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.Nama);
+        etTtl = (EditText) findViewById(R.id.Ttl);
+        etAlamat = (EditText) findViewById(R.id.Alamat);
+        bOK = (Button) findViewById(R.id.OK);
+        tvhasil = (TextView) findViewById(R.id.hasil);
+
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = etNama.getText().toString();
+                String Ttl = etTtl.getText().toString();
+                String Alamat = etAlamat.getText().toString();
+                tvhasil.setText(nama + " lahir di " + Ttl + " tinggal di ");
+
+            }
+        });
+
     }
 }
